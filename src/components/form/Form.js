7e5ea06e7181842d8a8dcbc25338c593fd019c23
Nina Petrova <petrova1nina@gmail.com>
2021-10-16 18:27:15 +0300

    Form validation added

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Icon from "../Icon";
 import Button from "../button/Button.js";
-import './Form.css'
 import classNames from "classnames";
+import './Form.css'
 
 export default function Form(props) {
 
     const buttonProps = [
         {
-            children: 'Save',
-            href: '/build-history',
+            onClick: () => {
+                console.log('Request success! Redirect to settings page');
+            },
             className: 'build-history-submit-btn action',
             disabled: false,
             active: true,
+            type: 'submit',
+            value: 'Save',
+            
         },
         {
             children: 'Cancel',
@@ -23,10 +27,10 @@ export default function Form(props) {
         },
     ]
     return (
-        <form>
+        <form action='/build-history'>
             <div className='form-group flex column'>
                 {props.fields.map(field => {
-                    let {label, placeholder, isRequired, type, ...attrs} = field;
+                    let { label, placeholder, isRequired, type, ...attrs } = field;
 
                     const name = label.replaceAll(' ', '-');
                     const classes = classNames(
@@ -36,28 +40,29 @@ export default function Form(props) {
                     );
                     return <div key={label} className={classes}>
                         {label &&
-                        <label
-                            htmlFor={name}
-                            className='form-label'>
-                            {label}
-                            {isRequired && <span className='required-mark'> *</span>}
-                        </label>}
+                            <label
+                                htmlFor={name}
+                                className='form-label'>
+                                {label}
+                                {isRequired && <span className='required-mark'> *</span>}
+                            </label>}
                         <input
                             name={name}
                             id={name} type={type ? type : 'text'}
                             className='form-input' placeholder={placeholder}
-                            required={isRequired && ''}
+                            required={isRequired && true}
+                            maxLength='50'
                             {...attrs.inputExtraParams}
                         />
                         {attrs.hasIcon &&
-                        <Icon className='input-clear-icon' direct={'icons/clear.svg'} itemName={'clear'}/>}
+                            <Icon className='input-clear-icon' direct={'icons/clear.svg'} itemName={'clear'} />}
                         {attrs.text && attrs.text}
                     </div>
                 })}
                 <div className='form-buttons flex row'>
                     {
                         buttonProps.map(button => {
-                            return <Button key={button.type || button.children} params={button}/>
+                            return <Button key={button.type || button.children} params={button} />
                         })
                     }
                 </div>
