4a22c6baeffdf4c27cb03ab8f277881fea7713a2
Nina Petrova <petrova1nina@gmail.com>
2021-10-17 17:13:31 +0300

    Chanded ID of builds in store

--- src/store/buildsStore.js
+++ src/store/buildsStore.js
@@ -1,67 +1,65 @@
-import {nanoid} from "nanoid";
-
 export const buildsStore = [
     {
         status: 'done',
         statusTitle: 'add documentation for postgres scaler',
         branch: 'master',
         author: 'Vadim Makeev',
-        id: nanoid(),
+        id: 'zHuT1Fj',
     },
     {
         status: 'failed',
         statusTitle: 'Super cool UI kit for making websites that look like games',
         branch: 'master',
         author: 'Vlad Rasputin',
-        id: nanoid(),
+        id: 'c3cufbw',
     },
     {
         status: 'processing',
         statusTitle: 'upgrade typescript to 3.8',
         branch: 'master',
         author: 'Uliana Repina',
-        id: nanoid(),
+        id:  '8edr-bj',
     },
     {
         status: 'done',
         statusTitle: 'Merge branch "master" of github.com:jaywcjlove/awesome',
         branch: 'master',
         author: 'Philip Kirkorov',
-        id: nanoid(),
+        id: 'm5UFBEI',
     },
     {
         status: 'done',
         statusTitle: 'replace all `div` to `article`',
         branch: 'master',
         author: 'Vadim Makeev',
-        id: nanoid(),
+        id: 'AjPn-Wa', 
     },
     {
         status: 'processing',
         statusTitle: 'improved accessibility',
         branch: 'master',
         author: 'Uliana Repina',
-        id: nanoid(),
+        id: 'L1Zzxnw',
     },
     {
         status: 'done',
         statusTitle: 'add documentation for postgres scaler',
         branch: 'master',
         author: 'Philip Kirkorov',
-        id: nanoid(),
+        id: 'fVG8aKp',
     },
     {
         status: 'done',
         statusTitle: 'fix: upload 别片类型',
         branch: 'master',
         author: 'Philip Kirkorov',
-        id: nanoid(),
+        id: 'RshmJAi',
     },
     {
         status: 'done',
         statusTitle: 'Form item has default height align with form size',
         branch: 'master',
         author: 'Vadim Makeev',
-        id: nanoid(),
+        id: '6Cs5evI',
     }
 ]
